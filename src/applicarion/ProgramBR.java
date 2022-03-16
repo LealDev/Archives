44b0f626513a28ac1b262d84b6f9123ca3c355c7
LealDev <lealmaster0111@gmail.com>
2022-03-16 20:57:34 -0300

    Learn about archives in Java

---
+++ src/applicarion/ProgramBR.java
@@ -0,0 +1,39 @@
+package applicarion;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ProgramBR {
+
+	public static void main(String[] args) {
+		 String path = "c:\\temp\\in.txt";
+		 BufferedReader	br = null;
+		 FileReader fr = null;
+		 
+		 try {
+			fr =new FileReader(path);
+			br = new BufferedReader(fr);
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (Exception e) {
+			System.out.println("Error: "+ e.getMessage());
+			}finally {
+				try {
+					if (br != null) {
+						br.close();
+					}if (fr != null) {
+						fr.close();
+					}
+		 
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+	}
+}
+
+		
